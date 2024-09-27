062e222438f562710f6d2c244be4832c44f2d902
hf6420 <175292968+hf6420@users.noreply.github.com>
2024-09-27 22:26:13 +0800

    Update

--- src/session/serversession.cpp
+++ src/session/serversession.cpp
@@ -17,10 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <ctime>
-#include <iomanip>
-#include <sstream>
-
 #include "serversession.h"
 #include "proto/trojanrequest.h"
 #include "proto/udppacket.h"
